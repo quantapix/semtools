71eb6483ef51ce872fbf5d33558173ba80e2f50d
quantapix <quantapix@gmail.com>
2021-02-13 22:54:07 -0500

    qpx

--- rust2/target/rls/debug/deps/hello_world_distroless-2f691e30e54eac48.d
+++
@@ -1,5 +0,0 @@
-/Users/qpix/clone/semtools/sample/distroless/examples/rust/target/rls/debug/deps/hello_world_distroless-2f691e30e54eac48.rmeta: src/main.rs
-
-/Users/qpix/clone/semtools/sample/distroless/examples/rust/target/rls/debug/deps/hello_world_distroless-2f691e30e54eac48.d: src/main.rs
-
-src/main.rs:
--- rust2/target/rls/debug/deps/hello_world_distroless-71e999f3a24995c6.d
+++
@@ -1,5 +0,0 @@
-/Users/qpix/clone/semtools/sample/distroless/examples/rust/target/rls/debug/deps/hello_world_distroless-71e999f3a24995c6.rmeta: src/main.rs
-
-/Users/qpix/clone/semtools/sample/distroless/examples/rust/target/rls/debug/deps/hello_world_distroless-71e999f3a24995c6.d: src/main.rs
-
-src/main.rs:
